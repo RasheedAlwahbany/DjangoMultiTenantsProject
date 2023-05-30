c93317e8d773e799770e90f4cbd7cd878d26b1e2
Rasheed Alwahbany <rasheedalwahbany@gmail.com>
2023-05-30 11:35:49 +0300

    Complete running the subdomain-tenant

--- DjangoMultiTenants/urls.py
+++ DjangoMultiTenants/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tenant import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('members/', views.get_members,name='members'),
 ]
--- tenant/views.py
+++ tenant/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .utilities import get_tenant
+from .models import Member
 
 # Create your views here.
+def get_members(request):
+    tenant = get_tenant(request)
+    members = Member.objects.filter(tenant=tenant).all()
+    return render(request, 'members.html', {'tenant':tenant,'members': members})
+    
